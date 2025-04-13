824c7dff6431ce0e1003cda0ea4681db9c3da5ef
Ilia Kurlovich <ikurlovich@icloud.com>
2025-04-13 19:05:49 +0400

    Added empty project body

---
+++ MaxBoots/MaxBootsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaxBootsApp.swift
+//  MaxBoots
+//
+//  Created by Ilia Kurlovich on 13.04.25.
+//
+
+import SwiftUI
+
+@main
+struct MaxBootsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
